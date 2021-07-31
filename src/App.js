af859911f5024f2480f09f77666ddb3bc1c22716
lesrz <suarezarnaldi.leo@gmail.com>
2021-07-31 20:15:50 -0300

    first build of myHub

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './App.css';
+// Imports components
+import Form from './components/Form';
+import TodoList from './components/Todolist';
+
+function App() {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className='App'>
+      <header>
+      <h1>myHub</h1>
+      </header>
+      <Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText} />
+      <TodoList todos={todos} setTodos={setTodos} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todolist.js
@@ -0,0 +1,23 @@
+import React from 'react';
+// Imports components
+import Todo from './Todo';
+
+const TodoList = ({ todos, setTodos }) => {
+    return (
+        <div className="todo-container">
+            <ul className="todo-list">
+                {todos.map((todo) => (
+                    <Todo 
+                        todo={todo} 
+                        todos={todos}  
+                        setTodos={setTodos} 
+                        key={todo.id} 
+                        text={todo.text} 
+                    />
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default TodoList; 
